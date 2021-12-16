f21fcee1d64e5e4226f4fb5e0252efd092c3dd51
abarar <38218691+lazy-geek@users.noreply.github.com>
2021-12-16 22:10:46 +0530

    Add Task Component

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+import Task from './lib/Task.svelte';
+let tasks = [
+	{'content': 'Task 1', 'isCheked': false},
+	{'content': 'Task 2', 'isCheked': false},
+	{'content': 'Task 3', 'isCheked': false},
+	{'content': 'Task 4', 'isCheked': false}
+	];
+
+	function addTask(e) {
+		tasks = [...tasks,{'content':e.detail.text,'isCheked':false}];
+	}
+</script>
+
+<main>
+	<h1>Todo List</h1>
+	{#each tasks as task , i }
+	<Task content={task.content} bind:isChecked = {task.isChecked} id={i}/>
+	{/each}
+
+</main>
---
+++ src/lib/Task.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let isChecked = false;
+    export let content = '';
+    export let id;
+</script>
+
+<div class='task'>
+    <input type="checkbox" name={id} id={id} bind:checked={isChecked}>
+    <label for={id} class="content">{content}</label>
+</div>
